1f4c5742ad29b7b9eb59f75f8914900316653399
Javier Serrano <rantes.javier@gmail.com>
2021-03-03 11:42:37 -0500

    set main class
    set main class
    
    set main class
    
    set main class
    
    Mergin commits

---
+++ linter.php
@@ -0,0 +1,46 @@
+<?php
+/** 
+ * Profiling purposes, DO NOT DELETE THIS LINES
+ */
+// tideways_xhprof_enable(TIDEWAYS_XHPROF_FLAGS_MEMORY | TIDEWAYS_XHPROF_FLAGS_CPU);
+
+/**
+ * Will perform linter on files
+ */
+Class Linter {
+    /**
+     * @todo Should set paths, configs and files to linter
+     */
+    public function __construct() {}
+    /**
+     * Will be executed after instantiation
+     */
+    public function Run() {
+        $this->_logger('Will perfomr linter!!!');
+    }
+    /**
+     * just perform a log system
+     * @param string $source 
+     */
+    private function _logger(string $message) {
+        $logdir = '/tmp/';
+        $file = 'PHPEslinter.log';
+        $stamp = date('d-m-Y i:s:H');
+
+        file_exists("{$logdir}{$file}") and filesize("{$logdir}{$file}") >= 524288000 and rename("{$logdir}{$file}", "{$logdir}{$stamp}_{$file}");
+        fwrite(STDOUT, "{$message}\n");
+        file_put_contents("{$logdir}{$file}", "[{$stamp}] - {$message}\n", FILE_APPEND);
+    }
+}
+
+$Linter = new Linter();
+$Linter->Run();
+
+/** Profiling purposes */
+// $data = tideways_xhprof_disable();
+// $file = 'PHPEslinter.xhprof';
+// file_put_contents(
+//     sys_get_temp_dir()."/{$file}",
+//     serialize($data)
+// );
+?>
